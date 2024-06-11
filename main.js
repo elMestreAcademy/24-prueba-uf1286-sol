914779ff550cfab2907a49b33b67fbcadd7d7536
Carlos Mestre <carmesce@gmail.com>
2024-06-11 13:52:24 +0200

    tabla html

--- main.js
+++ main.js
@@ -15,26 +15,28 @@ function generarArrayBidimensional() {
 }
 
 function mostrarArrayBidimensional(arrayBidimensional) {
+
+
   function mostrarArray(array) {
+    var temporal = "";
     array.forEach(function (elemento) {
       var formatedElemento = (" " + elemento).slice(-2);
-      process.stdout.write(`| ${formatedElemento} `);
+      temporal += ("<td>" + elemento + "</td>")
     });
     console.log("|");
+
+    return temporal
   }
 
-  function mostrarLinea() {
-    for (var i = 0; i < 15; i++) {
-      process.stdout.write("-");
-    }
-    console.log("-");
-  }
 
-  mostrarLinea();
+  document.getElementById("table").innerHTML += "<tr>";
   arrayBidimensional.forEach(function (array) {
-    mostrarArray(array);
-    mostrarLinea();
+    document.getElementById("table").innerHTML += "<tr>";
+    document.getElementById("table").innerHTML += mostrarArray(array); 
+    document.getElementById("table").innerHTML += "</tr>";
+
   });
+  document.getElementById("table").innerHTML += "</tr>";
 }
 
 mostrarArrayBidimensional(generarArrayBidimensional());
